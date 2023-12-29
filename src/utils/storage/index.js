d82cf89af9eeaf29d14eebc1710f579f5159b73a
Daniel Soto <dasg.1993@gmail.com>
2023-12-29 12:38:12 -0500

    Add CRUD operations for tasks in API route

--- src/utils/storage/index.js
+++ src/utils/storage/index.js
@@ -28,3 +28,34 @@ export const setStorage = (key, value) => {
     }
   }
 };
+
+export const removeStorage = (key, id) => {
+  if (typeof window !== "undefined" && window.localStorage) {
+    try {
+      const item = JSON.parse(localStorage.getItem(key));
+      const newItem = item.filter((task) => task.id !== id);
+      window.localStorage.setItem(key, JSON.stringify(newItem));
+      return newItem;
+    } catch (error) {
+      console.error(`Error removing localStorage key “${key}”:`, error);
+    }
+  }
+};
+
+export const updateStorage = (key, id, value) => {
+  if (typeof window !== "undefined" && window.localStorage) {
+    try {
+      const item = JSON.parse(localStorage.getItem(key));
+      const newItem = item.map((task) => {
+        if (task.id === id) {
+          return { ...task, ...value };
+        }
+        return task;
+      });
+      window.localStorage.setItem(key, JSON.stringify(newItem));
+      return newItem;
+    } catch (error) {
+      console.error(`Error updating localStorage key “${key}”:`, error);
+    }
+  }
+};
